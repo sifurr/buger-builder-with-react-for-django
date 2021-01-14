60b2fa605a8a278e372dab05da9c0373f8013667
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-15 00:35:38 +0600

    Project has been completed with Token Authorization feature

---
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Card, CardBody, CardHeader, Button, CardFooter } from 'reactstrap';
+
+const controls = [
+    { label: 'salad', type: 'salad' },
+    { label: 'cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+]
+
+const BuildControl = props => {
+    return (
+        <div className="d-flex">
+            <div className="mr-auto ml-5" style={{ fontSize: "1.2rem", fontWeight: "bold" }}>{props.label}</div>
+            <button className="btn btn-danger btn-sm m-1" onClick={props.removed}>Less</button>
+            <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
+        </div>
+    )
+}
+
+const Controls = props => {
+    return (
+        <div className="container ml-md-5" style={{ textAlign: "Center" }}>
+            <Card style={{
+                marginTop: "30px",
+                marginBottom: "30px",
+                textAlign: "center"
+            }}>
+                <CardHeader style={{ backgroundColor: "#D70F64", color: "#FFF" }}>
+                    <h4>Add Ingredients</h4>
+                </CardHeader>
+                <CardBody>
+                    {
+                        controls.map(item => {
+                            return <BuildControl
+                                label={item.label}
+                                type={item.type}
+                                key={Math.random()}
+                                added={() => props.ingredientAdded(item.type)}
+                                removed={() => props.ingredientRemoved(item.type)}
+                            />
+                        })
+                    }
+                </CardBody>
+                <CardFooter><h5>Price: <strong>{props.price}</strong> BDT</h5></CardFooter>
+                <Button style={{ backgroundColor: "#D70F64" }} disabled={!props.purchasable} onClick={props.toggleModal}>Order Now</Button>
+            </Card>
+        </div>
+    );
+}
+
+export default Controls;
---
+++ src/redux/authActionCreators.js
@@ -0,0 +1,116 @@
+import * as actionTypes from './actionTypes'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode'
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        payload: {
+            token: token,
+            userId: userId
+        }
+    }
+}
+
+export const authLoading = isLoading => {
+    return {
+        type: actionTypes.AUTH_LOADING,
+        payload: isLoading
+    }
+}
+
+export const authFailed = errMsg => {
+    return {
+        type: actionTypes.AUTH_FAILED,
+        payload: errMsg
+    }
+}
+
+const storeLocally = token => {
+    const decoded = jwt_decode(token);
+    const user_id = decoded.user_id;
+    const expTime = decoded.exp;
+
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', user_id);
+    const expirationTime = new Date(expTime * 1000);
+    localStorage.setItem('expirationTime', expirationTime);
+
+    return user_id
+}
+
+export const auth = (email, password, mode) => dispatch => {
+    dispatch(authLoading(true));
+    const authData = {
+        email: email,
+        password: password
+    }
+
+    const header = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    let authUrl = null;
+    if (mode === "Sign Up") {
+        authUrl = "http://127.0.0.1:8000/api/user/";
+    } else {
+        authUrl = "http://127.0.0.1:8000/api/token/"
+    }
+
+    axios.post(authUrl, authData, header)
+        .then(response => {
+            dispatch(authLoading(false));
+            if (mode !== "Sign Up") {
+                const token = response.data.access;
+                const user_id = storeLocally(token)
+                dispatch(authSuccess(token, user_id));
+            } else {
+                dispatch(authLoading(true))
+                return axios.post("http://127.0.0.1:8000/api/token/", authData, header)
+                    .then(response => {
+                        dispatch(authLoading(false));
+                        const token = response.data.access;
+                        const user_id = storeLocally(token)
+                        dispatch(authSuccess(token, user_id));
+                    })
+            }
+
+            console.log(response);
+        })
+        .catch(err => {
+            dispatch(authLoading(false));
+            const key = Object.keys(err.response.data)[0];
+            console.log(err.response.data[key]);
+            dispatch(authFailed(`${key.toUpperCase()}: ${err.response.data[key]}`));
+        })
+}
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    localStorage.removeItem('userId');
+    return {
+        type: actionTypes.AUTH_LOGOUT,
+
+    }
+}
+
+
+export const authCheck = () => dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        //Logout
+        dispatch(logout());
+    } else {
+        const expirationTime = new Date(localStorage.getItem('expirationTime'));
+        if (expirationTime <= new Date()) {
+            // Logout
+            dispatch(logout());
+        } else {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+        }
+    }
+}
